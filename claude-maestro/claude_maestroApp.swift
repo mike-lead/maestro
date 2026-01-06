f24fb9285c47b7dfe6a6d120a05e3c852154c511
AngryAnt3201 <jackwakem@gmail.com>
2026-01-06 14:42:50 +1100

    Initial Commit

---
+++ claude-maestro/claude_maestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  claude_maestroApp.swift
+//  claude-maestro
+//
+//  Created by Jack Wakem on 6/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct claude_maestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
